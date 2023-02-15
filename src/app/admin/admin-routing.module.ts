8702e9f7683ed1ab4a925f9eeb6eadc611648fe4
Naresh kumar <nareshkumarsoundarraji29@gmail.com>
2023-02-15 21:56:44 +0530

    hello

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,6 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { InputComponent } from './about/input/input.component';
+import { OutputComponent } from './about/output/output.component';
 import { AdminComponent } from './admin.component';
+import { ContactComponent } from './contact/contact.component';
+import { FlexComponent } from './contact/flex/flex.component';
+import { GridComponent } from './contact/grid/grid.component';
+import { ComponentComponent } from './dashboard/component/component.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './dashboard/menu/menu.component';
+import { CompassComponent } from './design/compass/compass.component';
+import { DesignComponent } from './design/design.component';
+import { CardComponent } from './gallery/card/card.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageComponent } from './gallery/image/image.component';
+import { ContentComponent } from './home/content/content.component';
+import { FolderComponent } from './home/folder/folder.component';
+import { HomeComponent } from './home/home.component';
+import { AddressComponent } from './service/address/address.component';
+import { SelectionComponent } from './service/selection/selection.component';
+import { ServiceComponent } from './service/service.component';
 
 
 const routes: Routes = [
@@ -8,13 +28,62 @@ const routes: Routes = [
     path: '',
     component: AdminComponent,
     children: [
-      { path: 'dashboard', loadChildren: () => import('../admin/dashboard/dashboard.module').then(m => m.DashboardModule)},
-      { path: 'home', loadChildren: () => import('../admin/home/home.module').then(m => m.HomeModule) },
-      { path: 'about', loadChildren: () => import('../admin/about/about.module').then(m => m.AboutModule) },
-      { path: 'design', loadChildren: () => import('../admin/design/design.module').then(m => m.DesignModule) },
-      { path: 'gallery', loadChildren: () => import('../admin/gallery/gallery.module').then(m => m.GalleryModule) },
-      { path: 'service', loadChildren: () => import('../admin/service/service.module').then(m => m.ServiceModule) },
-      { path: 'contact', loadChildren: () => import('../admin/contact/contact.module').then(m => m.ContactModule) },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        children: [
+          { path: 'menu', component: MenuComponent },
+          { path: 'component', component: ComponentComponent }
+        ]
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+          { path: 'content', component: ContentComponent },
+          { path: 'folder', component: FolderComponent }
+        ]
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+        children: [
+          { path: 'input', component: InputComponent },
+          { path: 'output', component: OutputComponent }
+        ]
+      },
+      {
+        path: 'design',
+        component: DesignComponent,
+        children: [
+          { path: 'compass', component: CompassComponent },
+          { path: 'title', component: FlexComponent }
+        ]
+      },
+      {
+        path: 'gallery',
+        component: GalleryComponent,
+        children: [
+          { path: 'card', component: CardComponent },
+          { path: 'image', component: ImageComponent }
+        ]
+      },
+      {
+        path: 'service',
+        component: ServiceComponent,
+        children: [
+          { path: 'address', component: AddressComponent },
+          { path: 'selection', component: SelectionComponent }
+        ]
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+        children: [
+          { path: 'flex', component: FlexComponent },
+          { path: 'grid', component: GridComponent }
+        ]
+      },
     ]
   },
 ];
